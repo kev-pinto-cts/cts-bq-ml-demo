875b075376a7195ed0e2548a3a0cee3a6e563225
kev-pinto-cts <kev.pinto@cts.co>
2023-05-20 06:42:37 +0000

    Stable Dev Con

---
+++ {{ cookiecutter.project_name }}/infra/providers.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = "~> 1.3"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.52"
+    }
+    {% if cookiecutter.vcs == "github" -%}
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.17"
+    }
+    {%- elif cookiecutter.vcs == "gitlab" -%}
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "~> 15"
+    }
+    {%- elif cookiecutter.vcs == "bitbucket-cloud" -%}
+    bitbucket = {
+      source  = "DrFaust92/bitbucket"
+      version = "~> 2"
+    }
+    {%- endif %}
+  }
+  backend "gcs" {
+    bucket = "REPLACE-ME"
+    prefix = "terraform/mgmt"
+  }
+}
+
+provider "{{ cookiecutter.vcs | replace("-cloud", "") }}" {
+  {% if cookiecutter.vcs != "bitbucket-cloud" -%}
+  token = chomp(data.google_secret_manager_secret_version.access_token.secret_data)
+  {%- endif %}
+  {% if cookiecutter.vcs == "github" -%}
+  owner = "{{ cookiecutter.repo_owner }}"
+  {%- endif %}
+  {% if cookiecutter.vcs == "bitbucket-cloud" -%}
+  username = "{{ cookiecutter.bitbucket_username }}"
+  password = chomp(data.google_secret_manager_secret_version.access_token.secret_data)
+  {%- endif %}
+}
