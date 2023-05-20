0825f437dc2e1ea91b3a20cdf66cc06a1d4bc374
kev-pinto-cts <kev.pinto@cts.co>
2023-05-20 09:57:56 +0000

    Terraform plan

--- {{ cookiecutter.project_name }}/infra/providers.tf
+++ {{ cookiecutter.project_name }}/infra/providers.tf
@@ -3,24 +3,8 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.52"
+      version = "4.63.1"
     }
-    {% if cookiecutter.vcs == "github" -%}
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.17"
-    }
-    {%- elif cookiecutter.vcs == "gitlab" -%}
-    gitlab = {
-      source  = "gitlabhq/gitlab"
-      version = "~> 15"
-    }
-    {%- elif cookiecutter.vcs == "bitbucket-cloud" -%}
-    bitbucket = {
-      source  = "DrFaust92/bitbucket"
-      version = "~> 2"
-    }
-    {%- endif %}
   }
   backend "gcs" {
     bucket = "REPLACE-ME"
@@ -28,15 +12,8 @@ terraform {
   }
 }
 
-# provider "{{ cookiecutter.vcs | replace("-cloud", "") }}" {
-#   {% if cookiecutter.vcs != "bitbucket-cloud" -%}
-#   token = chomp(data.google_secret_manager_secret_version.access_token.secret_data)
-#   {%- endif %}
-#   {% if cookiecutter.vcs == "github" -%}
-#   owner = "{{ cookiecutter.repo_owner }}"
-#   {%- endif %}
-#   {% if cookiecutter.vcs == "bitbucket-cloud" -%}
-#   username = "{{ cookiecutter.bitbucket_username }}"
-#   password = chomp(data.google_secret_manager_secret_version.access_token.secret_data)
-#   {%- endif %}
-# }
+provider "google" {
+  # Configuration options
+  project = var.project_id
+  region  = var.region
+}
