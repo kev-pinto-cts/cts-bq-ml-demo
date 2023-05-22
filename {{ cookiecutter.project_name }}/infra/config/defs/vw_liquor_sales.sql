54e7d7426c685b0074f7c3217825aa7dd48d9ddf
kev-pinto-cts <kev.pinto@cts.co>
2023-05-22 11:43:24 +0000

    Addition of Reworked Query

--- {{ cookiecutter.project_name }}/infra/config/defs/vw_liquor_sales.sql
+++ {{ cookiecutter.project_name }}/infra/config/defs/vw_liquor_sales.sql
@@ -1,10 +1,6 @@
 SELECT
-  date AS day,
-  item_description AS item_name,
-  SUM(bottles_sold) AS total_amount_sold
-FROM
-  `iowa_demo.sales`
-WHERE
-  date BETWEEN '2021-01-01'
-  AND '2022-06-30'
-GROUP BY 1
+ date as day,
+ item_description AS item_name,
+ SUM(bottles_sold) AS total_amount_sold
+FROM `iowa_demo.sales`
+WHERE date BETWEEN '2021-01-01' AND '2022-06-30'
